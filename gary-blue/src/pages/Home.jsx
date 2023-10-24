2d9dafd67e168e0c1a3638b69329978cc4dfeadf
NLScotty <Scott.B.Normore@gmail.com>
2023-10-24 16:34:07 -0230

    Ethan Millers Changes
    A commit containing Ethan's changes

--- gary-blue/src/pages/Home.jsx
+++ gary-blue/src/pages/Home.jsx
@@ -1,11 +1,22 @@
-import React, {useState} from 'react'
+function Home(){
 
-function Home() {
-  return (
-    <div>
-        <p>This is the Home Page</p>
-    </div>
-  )
+  return(
+<>
+  <h2 className="Text">Gary Blue's Halloween Diner</h2>
+  
+      <h3 className="Blackbox">
+          <div className="Greybox"><span className="Redbox">Gary Blue's Halloween Diner, a wonderful place to eat!</span></div>
+          
+          <p className="Orangetext">We are located on 123 Scary Road.</p>
+         
+         
+          <p className="Hourstext">Our Hours of Operation:
+                                           Monday to Friday:5am-8pm |
+                                           Saturday and Sunday:Closed</p>
+          </h3>
+      
+</>
+  );
 }
 
-export default Home
+export default Home;
