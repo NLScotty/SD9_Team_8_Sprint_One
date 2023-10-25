c8f8178e251b6a6f3a9ba2775c7206e5d84fce39
NLScotty <Scott.B.Normore@gmail.com>
2023-10-25 20:46:32 -0230

    Completed Desktop Bar
    Completed the Desktop Bar. Mobile Nav Menu Completed. TODO: Fix Mobile Nav Bar Header

--- gary-blue/src/components/Navbar.jsx
+++ gary-blue/src/components/Navbar.jsx
@@ -1,17 +1,50 @@
 import React, {useState} from 'react'
+import hamburgerIcon from '../images/Hamburger_Menu_Icon.png'
 
 function Navbar({func}) {
+  function overlayMenu(){
+    let overlay = document.querySelector('.Mobile_Nav_Menu_Content');
+    if(overlay.style.display === "none"){
+      overlay.style.display = "block";
+    }else{
+      overlay.style.display = "none";
+    }
+  }
   return (
-    <div>
-      <button onClick={() => func("Home")}>
-        Home Page
-      </button>
-      <button onClick={() => func("Menu")}>
-        Menu Page
-      </button>
-      <button onClick={() => func("Order")}>
-        Order Page
-      </button>
+    <div className='Nav_Bar'>
+      <div className="Desktop_Navigation_Bar">
+        <div className='Desktop_Nav_Logo'>
+          <div className='Desktop_Nav_Logo_Container'>
+            Gary Boo's Halloween Diner
+          </div>
+        </div>
+        <div className='Desktop_Nav_Buttons'>
+          <div className='Desktop_Nav_Button' onClick={() => func("Home")}>
+            <div className='Desktop_Nav_Button_Label'> Home </div>
+          </div>
+          <div className='Desktop_Nav_Button' onClick={() => func("Menu")}>
+            <div className='Desktop_Nav_Button_Label'> Menu </div>
+          </div>
+          <div className='Desktop_Nav_Button' onClick={() => func("Order")}>
+            <div className='Desktop_Nav_Button_Label'> Order </div>
+          </div>
+        </div>
+      </div>
+      <div className='Mobile_Nav_Bar'>
+        <div className="Mobile_Nav_Dropdown_Menu">
+          <button className="Mobile_Nav_Menu_Button" onClick={overlayMenu}>
+            <img src={hamburgerIcon} alt="Menu_Icon"/>
+          </button>
+          <div className="Mobile_Nav_Menu_Content">
+            <div className="Mobile_Nav_Menu_Item" onClick={() => {func("Home"); overlayMenu()}}>Home</div>
+            <div className="Mobile_Nav_Menu_Item" onClick={() => {func("Menu"); overlayMenu()}}>Menu</div>
+            <div className="Mobile_Nav_Menu_Item" onClick={() => {func("Order"); overlayMenu()}}>Order</div>
+          </div>
+        </div>
+        <div className="Mobile_Nav_Bar_Header">
+          Gary Boo's Halloween Diner
+        </div>
+      </div>
     </div>
   )
 }
--- gary-blue/src/pages/Home.jsx
+++ gary-blue/src/pages/Home.jsx
@@ -1,21 +1,16 @@
 function Home(){
 
   return(
-<>
-  <h2 className="Text">Gary Blue's Halloween Diner</h2>
-  
-      <h3 className="Blackbox">
-          <div className="Greybox"><span className="Redbox">Gary Blue's Halloween Diner, a wonderful place to eat!</span></div>
-          
-          <p className="Orangetext">We are located on 123 Scary Road.</p>
-         
-         
+  <>
+    <h3 className="Blackbox">
+      <div className="Greybox"><span className="Redbox">Gary Boo's Halloween Diner, a wonderful place to eat!</span></div>
+        <p className="Orangetext">We are located on 123 Scary Road.</p> 
           <p className="Hourstext">Our Hours of Operation:
                                            Monday to Friday:5am-8pm |
                                            Saturday and Sunday:Closed</p>
           </h3>
       
-</>
+  </>
   );
 }
 
